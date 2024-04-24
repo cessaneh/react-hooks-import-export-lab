4b74f35db3239852b23404e69fd043a360714edd
princess <princessmumbi001@gmail.com>
2024-04-24 12:55:03 +0300

    import export lab complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {username,city} from "../data/user"
 function Home() {
   return (
     <div id="home">
@@ -8,4 +8,7 @@ function Home() {
       </h1>
     </div>
   );
+  
 }
+export default Home;
+
